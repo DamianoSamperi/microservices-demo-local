18950c644a11d44b1b68e693f81fcc9f7dec2faf
DamianoSamperi <102314677+DamianoSamperi@users.noreply.github.com>
2025-07-05 13:18:15 +0200

    Update productmanagementserver.go

--- src/productmanagementservice/productmanagementserver.go
+++ src/productmanagementservice/productmanagementserver.go
@@ -30,8 +30,10 @@ type server struct {
 func (s *server) AddProduct(ctx context.Context, req *pb.AddProductRequest) (*pb.AddProductResponse, error) {
 	// Genera embedding
 	textToEmbed := req.Name + " " + req.Description
-
-	embedResp, err := s.embeddingClient.GenerateEmbedding(ctx, &embedpb.EmbeddingRequest{Text: textToEmbed})
+	
+	embedResp, err := s.embeddingClient.GenerateEmbedding(ctx, &embedpb.EmbeddingRequest{
+		Image: req.Picture, // supponendo che `picture` ora sia []byte e non string
+	})
 	if err != nil {
 		return &pb.AddProductResponse{Success: false, Message: "embedding service error: " + err.Error()}, nil
 	}
