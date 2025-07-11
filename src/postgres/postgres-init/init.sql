771e553be1cc4f79cce6cba2ea9d7b7cf31b815b
DamianoSamperi <damihearts@hotmail.it>
2025-07-11 12:06:46 +0200

    update init

--- src/postgres/postgres-init/init.sql
+++ src/postgres/postgres-init/init.sql
@@ -18,5 +18,7 @@ CREATE TABLE catalog_items (
   price_usd_currency_code TEXT,
   price_usd_units INTEGER,
   price_usd_nanos BIGINT,
-  categories TEXT
+  categories TEXT,
+  product_embedding VECTOR(384),
+  embed_model TEXT
 );
